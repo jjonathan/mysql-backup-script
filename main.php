1ceda99ff59ddc47e3b8b412ead2811997f2b0c1
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-03-22 13:55:23 -0300

    log messages

--- main.php
+++ main.php
@@ -8,7 +8,11 @@
 	$options = file_get_contents('options.json');
 	$options = json_decode($options);
 
+	echo_log("Starting");
+
 	log_define();
 	backup($options);
 	clean();
+	
+	echo_log("Done");
 ?>
